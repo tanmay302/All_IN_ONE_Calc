b6ef132185b294af04f9a81fa6ec66327e3a8bc0
Tanmay Khedekar <tanmaykhedekar6803@gmail.com>
2024-09-15 20:02:50 +0530

    Add files via upload

---
+++ js/calc.js
@@ -0,0 +1,40 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+
+let weight=document.getElementsByClassName("ip");
+let calc=document.getElementById("calcuateWgt");
+
+calc.addEventListener("click", (event) =>{
+    console.log(weight);
+});
+
+
+ 
